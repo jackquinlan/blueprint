5dbe34824da200fabcb60b904ae0ffa35049a54e
jackquinlan <jquinlan@maxpayload.com>
2023-11-14 13:20:27 -0500

    feat: add license

--- apps/web/src/app/_components/footer.tsx
+++ apps/web/src/app/_components/footer.tsx
@@ -9,7 +9,7 @@ export function Footer() {
         <footer className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
             <div className="flex flex-col items-center gap-4 px-8 sm:flex-row md:gap-2 md:px-0">
                 <Shapes className="h-5 w-5" />
-                <div className="flex items-center gap-1">
+                <div className="flex flex-col items-center gap-1 sm:flex-row">
                     <p className="text-center text-sm leading-loose md:text-left">
                         Built by{" "}
                         <a
