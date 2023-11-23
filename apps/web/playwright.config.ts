afaac1f5905e4fb54374123dd1c9fea27bd631f7
Mohsen Azimi <mohsen.azimi@airbnb.com>
2023-11-23 16:31:59 +0100

    Add GitHub Action for Playwright

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -3,6 +3,10 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
     testDir: "e2e-tests",
     reporter: "html",
+    webServer: {
+        command: "pnpm dev",
+        port: 3000,
+    },
     use: {
         baseURL: "http://localhost:3000",
     },
